077d2557549d51a03cbb9d3e41af882bfaa6f0ed
Kellie Manasco <kellie.manasco@gmail.com>
2021-03-04 14:10:55 -0500

    adds functionality to all operators. Still working out minor bugs, but seems to be working overall

--- script.js
+++ script.js
@@ -9,29 +9,29 @@ const power = (a, b) => Math.pow(a, b);
 
 function operate(o) {
     if (o == 'add') {
-        stored = parseInt(current) + stored;
+        stored = parseFloat(stored) + parseInt(current);
         document.querySelector('.display').innerHTML = stored;
         current = 0;
     }
-    else if (operator = 'sub') {
-        current = parseInt(current) - stored;
-        stored = current;
+    else if (o == 'sub') {
+        stored = parseFloat(stored) - parseInt(current);
         document.querySelector('.display').innerHTML = stored;
+        current = 0;
     }
-    else if (operator = 'mul') {
-        current = parseInt(current) + stored;
-        stored = current;
+    else if (o == 'mul') {
+        stored = parseFloat(stored) * parseInt(current);
         document.querySelector('.display').innerHTML = stored;
+        current = 0;
     }
-    else if (operator = 'div') {
-        current = parseInt(current) + stored;
-        stored = current;
+    else if (o == 'div') {
+        stored = parseFloat(stored) / parseInt(current);
         document.querySelector('.display').innerHTML = stored;
+        current = 0;
     }
-    else if (operator = 'pow') {
-        current = parseInt(current) + stored;
-        stored = current;
+    else if (o == 'pow') {
+        stored = Math.pow(parseFloat(stored), parseInt(current));
         document.querySelector('.display').innerHTML = stored;
+        current = 0;
     }
 }
 
@@ -45,19 +45,42 @@ document.querySelector('.add').onclick = function() {
 }
 document.querySelector('.sub').onclick = function() {
     if (operator !== 'none') {
-    operate(operator);}
+    operate(operator);
+    }
+    else {stored = current;}
     operator = 'sub';
+    current = 0;
 }
 document.querySelector('.mul').onclick = function() {
     if (operator !== 'none') {
-    operate(operator);}
+        operate(operator);
+    }
+    else {stored = current;}
     operator = 'mul';
+    current = 0;
 }
 document.querySelector('.div').onclick = function() {
     if (operator !== 'none') {
-    operate(operator);}
+        operate(operator);
+    }
+    else {stored = current;}
     operator = 'div';
+    current = 0;
 }
+document.querySelector('.pow').onclick = function() {
+    if (operator !== 'none') {
+        operate(operator);
+    }
+    else {stored = current;}
+    operator = 'pow';
+    current = 0;
+}
+document.querySelector('.eql').onclick = function() {
+    operate(operator);
+    operator = 'none';
+    current = stored;
+}
+
 
 document.querySelector('.display').innerHTML = current;
 
@@ -69,31 +92,23 @@ document.querySelector('.two').onclick = function() {
     current === 0 ? current = 2 : current += '2';
     document.querySelector('.display').innerHTML = current;}
 
-    document.querySelector('.three').onclick = function() {
+document.querySelector('.three').onclick = function() {
     current === 0 ? current = 3 : current += '3';
     document.querySelector('.display').innerHTML = current;}
 
-    document.querySelector('.four').onclick = function() {
+document.querySelector('.four').onclick = function() {
     current === 0 ? current = 4 : current += '4';
     document.querySelector('.display').innerHTML = current;}
 
-    document.querySelector('.five').onclick = function() {
-        if (current === 'inactive') {
-            document.querySelector('.display').innerHTML = stored;
-        }
-        else if (current === 0) {
-            current = 5
-        }
-        else {current += '5';
-    document.querySelector('.display').innerHTML = current;
-        }
-}
-
-    document.querySelector('.six').onclick = function() {
+document.querySelector('.five').onclick = function() {
+    current === 0 ? current = 5 : current += '5';
+    document.querySelector('.display').innerHTML = current;}
+    
+document.querySelector('.six').onclick = function() {
     current === 0 ? current = 6 : current += '6';
     document.querySelector('.display').innerHTML = current;}
 
-    document.querySelector('.seven').onclick = function() {
+document.querySelector('.seven').onclick = function() {
     current === 0 ? current = 7 : current += '7';
     document.querySelector('.display').innerHTML = current;}
 
@@ -111,8 +126,15 @@ document.querySelector('.zero').onclick = function() {
     document.querySelector('.display').innerHTML = current;}
 
 document.querySelector('.posneg').onclick = function() {
+    if (current == 'inactive') {
+        stored != 0 ? stored = -stored : 0;
+    document.querySelector('.display').innerHTML = stored;
+    }
+    else {
     current != 0 ? current = -current : 0;
-    document.querySelector('.display').innerHTML = current;}
+    document.querySelector('.display').innerHTML = current;
+    }
+}
 
 document.querySelector('.ac').onclick = function() {
     current = 0;
@@ -121,7 +143,13 @@ document.querySelector('.ac').onclick = function() {
     document.querySelector('.display').innerHTML = current;}
 
 document.querySelector('.c').onclick = function() {
+    if (typeof current !== 'string') {
+        current = current.toString();
+    }
     current === 0 ? current = 0 : current = current.slice(0, -1);
+    if (current.length <= 0) {
+        current = 0;
+    }
     document.querySelector('.display').innerHTML = current;}
 
 document.querySelector('.dec').onclick = function() {
